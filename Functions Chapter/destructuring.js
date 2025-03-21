166b2e9a7e41d428e2297930434e89b7905790b6
Farhad Khamisipour <farhadkhamisipour@gmail.com>
2025-03-21 16:59:20 +0100

    Unpacking an object to parameters

--- Functions Chapter/destructuring.js
+++ Functions Chapter/destructuring.js
@@ -3,3 +3,13 @@ function vectorMultiply({ x, y }, scalar) {
     return { x: x * scalar, y: y * scalar };
 }
 vectorMultiply({ x: 1, y: 2 }, 2)  // => {x: 2, y: 4}
+
+// Vector addition example 
+function vectorAdd(
+    {
+        x: x1, y: y1  // Unpack 1st object into x1 and y1 params
+    },
+    {
+        x: x2, y: y2 // Unpack 2nd object into x2 and y2
+    }
+)
